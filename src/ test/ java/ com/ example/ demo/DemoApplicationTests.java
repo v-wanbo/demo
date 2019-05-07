efd5b8e3799cc33e01168502631bc41e4a5b3ce9
v-wanbo <49471307+v-wanbo@users.noreply.github.com>
2019-05-07 14:33:28 +0800

    Update DemoApplicationTests.java

--- src/ test/ java/ com/ example/ demo/DemoApplicationTests.java
+++ src/ test/ java/ com/ example/ demo/DemoApplicationTests.java
@@ -13,7 +13,7 @@ public class DemoApplicationTests {
 	 /*  17:18  */
 	@Test 
 	public void contextLoads() {
-		System.out.println("2019 年 下午 14点32分  测试");
+		int i=1/0;
 		System.out.println("Hello Word ！！！！！");
 	}
 
