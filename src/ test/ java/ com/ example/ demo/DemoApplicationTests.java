bf65d6f4090ed3a333de35216f152aca9e9bc495
v-wanbo <49471307+v-wanbo@users.noreply.github.com>
2019-05-08 14:10:05 +0800

    Update DemoApplicationTests.java

--- src/ test/ java/ com/ example/ demo/DemoApplicationTests.java
+++ src/ test/ java/ com/ example/ demo/DemoApplicationTests.java
@@ -10,12 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DemoApplicationTests {
 	
 	public static void main(String [] args){
-		System.out.println("2019 下午 14点40 测试 ！！！！！！！！！");	
+		System.out.println("2019 /05 /08 下午 14点09 测试 ！！！！！！！！！");	
 	}
-	 /*  17:18  */
+	 
 	@Test 
 	public void contextLoads() {
-		int i=1/0;
+ 
 		System.out.println("Hello Word ！！！！！");
 	}
 
